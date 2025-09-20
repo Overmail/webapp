e7c46a0e61eceb1263408e7c4baf1630374d9cd9
Julius Vincent Babies <julius@familie-babies.de>
2025-09-20 13:21:45 +0200

    Fullscreen Drawer on mobile + adapted padding on x/y when expanded

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-    import {fade} from "svelte/transition";
+    import {fade, slide} from "svelte/transition";
     import favicon from "$lib/assets/favicon.svg";
-    import {Home, LogOut, Pin} from "@lucide/svelte";
+    import {Home, LogOut, Menu, Pin, X} from "@lucide/svelte";
     import {browser} from "$app/environment";
     import Item from "./_menu/Item.svelte";
     import {onMount} from "svelte";
@@ -34,64 +34,81 @@
     ></div>
 
     <div
-            aria-label="Expand Drawer"
-            role="button"
-            tabindex="0"
-            class="absolute w-10 top-4 left-2 pl-2 rounded-lg flex flex-col gap-4 items-start justify-start z-10 transition-all duration-150 bg-[#f5f2e9] overflow-x-hidden"
-            style="height: calc(100% - 2 * 16px)"
-            onmouseenter={() => expandDrawer = true}
-            onmouseleave={() => pinDrawer ? null : expandDrawer = false}
-            class:w-56={expandDrawer || pinDrawer}
-            class:pr-2={expandDrawer || pinDrawer}
-            class:shadow-lg={expandDrawer && !pinDrawer}
+            class="absolute top-0 left-0 w-full h-full flex px-2 transition-all duration-150"
+            class:py-2={expandDrawer || pinDrawer}
+            class:py-4={!(expandDrawer || pinDrawer)}
     >
-        <div class="flex flex-col gap-4 mt-2 items-center overflow-clip w-full">
-            <img src={favicon} alt="Overmail Logo" class="w-8 h-8" />
-            <div class="h-6">
+        <div
+                aria-label="Expand Drawer"
+                role="button"
+                tabindex="0"
+                class="h-full w-10 pl-2 rounded-lg flex flex-col items-start justify-start z-10 transition-all duration-150 bg-[#f5f2e9] overflow-x-hidden"
+                onmouseenter={() => expandDrawer = true}
+                onmouseleave={() => pinDrawer ? null : expandDrawer = false}
+                class:w-full={expandDrawer || pinDrawer}
+                class:sm:w-56={expandDrawer || pinDrawer}
+                class:pr-2={expandDrawer || pinDrawer}
+                class:py-2={expandDrawer || pinDrawer}
+                class:shadow-lg={expandDrawer && !pinDrawer}
+        >
+            <div class="mt-2 w-full">
+                {#if !pinDrawer}
+                    <div transition:slide={{ duration: 150, axis: "y" }}>
+                        <Item
+                                slim={true}
+                                icon={expandDrawer ? X : Menu}
+                                onclick={() => expandDrawer = !expandDrawer}
+                                title="Menü einklappen" />
+                    </div>
+                {/if}
+            </div>
+
+            <div class="flex flex-row gap-2 items-center overflow-clip w-full">
+                <img src={favicon} alt="Overmail Logo" class="w-8 h-8" />
                 {#if expandDrawer || pinDrawer}
-                    <div class="text-2xl font-bold" transition:fade={{ duration: 100 }}>
+                    <div class="text-lg font-bold" transition:fade={{ duration: 100 }}>
                         Overmail
                     </div>
                 {/if}
             </div>
-        </div>
 
-        <div class="flex flex-col flex-1 gap-1 mt-2 w-full">
-            <!-- Drawer content -->
-
-            <Item
-                    class="mb-4"
-                    active={page.url.pathname === "/dashboard"}
-                    slim={true}
-                    icon={Home}
-                    onclick={() => goto("/dashboard")}
-                    title="Dashboard" />
-
-            {#each $folderTree as folder}
+            <div class="flex flex-col flex-1 gap-1 mt-2 w-full">
+                <!-- Drawer content -->
                 <Item
-                        active={page.url.pathname.startsWith(`/dashboard/folder/${folder.id}`)}
+                        class="mb-4"
+                        active={page.url.pathname === "/dashboard"}
                         slim={true}
-                        suffix={folder.unreadCount > 0 ? folder.unreadCount.toString() : undefined}
-                        showSuffix={folder.unreadCount > 0 && (expandDrawer || pinDrawer)}
-                        hasDot={folder.unreadCount > 0}
-                        icon={folder.getIcon()}
-                        onclick={() => goto(`/dashboard/folder/${folder.id}`)}
-                        title={folder.getDisplayName() ?? folder.name}
-                        subtitle={folder.getDisplayName() ? folder.name : undefined}
-                />
-            {/each}
-        </div>
+                        icon={Home}
+                        onclick={() => goto("/dashboard")}
+                        title="Dashboard" />
 
-        <div class="flex flex-col gap-1 mb-2 w-full">
-            <Item
-                onclick={() => pinDrawer = !pinDrawer}
-                title="Menü Pinnen"
-                icon={Pin} />
+                {#each $folderTree as folder}
+                    <Item
+                            active={page.url.pathname.startsWith(`/dashboard/folder/${folder.id}`)}
+                            slim={true}
+                            suffix={folder.unreadCount > 0 ? folder.unreadCount.toString() : undefined}
+                            showSuffix={folder.unreadCount > 0 && (expandDrawer || pinDrawer)}
+                            hasDot={folder.unreadCount > 0}
+                            icon={folder.getIcon()}
+                            onclick={() => goto(`/dashboard/folder/${folder.id}`)}
+                            title={folder.getDisplayName() ?? folder.name}
+                            subtitle={folder.getDisplayName() ? folder.name : undefined}
+                    />
+                {/each}
+            </div>
 
-            <Item
-                onclick={() => window.location.href = "/api/auth/logout"}
-                title="Abmelden"
-                icon={LogOut} />
+            <div class="flex flex-col gap-1 mb-2 w-full">
+                <Item
+                        class="hidden sm:flex"
+                        onclick={() => pinDrawer = !pinDrawer}
+                        title="Menü Pinnen"
+                        icon={Pin} />
+
+                <Item
+                        onclick={() => window.location.href = "/api/auth/logout"}
+                        title="Abmelden"
+                        icon={LogOut} />
+            </div>
         </div>
     </div>
 
